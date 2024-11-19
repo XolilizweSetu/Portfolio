310fb0cee68c4b05bfab32f5ba92c1d5f5be6526
XolilizweSetu <95090883+XolilizweSetu@users.noreply.github.com>
2024-11-19 09:52:19 -1000

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+const slides = document.querySelector('.slides');
+let index = 0;
+
+function showSlide() {
+    index++;
+    if (index >= slides.children.length) index = 0;
+    slides.style.transform = `translateX(-${index * 100}%)`;
+}
+
+setInterval(showSlide, 3000);
+
+
+  
